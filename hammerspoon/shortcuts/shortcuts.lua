c91d528e39611e342f97a6169ee3c65544ce6801
Bhushan <bhushangaykawad@gmail.com>
2022-02-28 09:49:50 +0530

    added more shortcuts

--- hammerspoon/shortcuts/shortcuts.lua
+++ hammerspoon/shortcuts/shortcuts.lua
@@ -8,6 +8,9 @@ end
 --- quick open applications
 hs.hotkey.bind({"cmd", "shift"}, "W", open_app("Webstorm"))
 hs.hotkey.bind({"cmd", "shift"}, "B", open_app("Brave Browser"))
+hs.hotkey.bind({"cmd", "shift"}, "G", open_app("Google Chrome"))
+hs.hotkey.bind({"cmd", "shift"}, "M", open_app("Microsoft Teams"))
+hs.hotkey.bind({"cmd", "shift"}, "O", open_app("Microsoft Outlook"))
 hs.hotkey.bind({"cmd", "shift"}, "T", open_app("iTerm"))
 hs.hotkey.bind({"cmd", "shift"}, "S", open_app("Slack"))
 hs.hotkey.bind({"cmd", "shift"}, "D", open_app("Discord"))
