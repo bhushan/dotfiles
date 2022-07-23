c5c80aed57530afc2cf3f997c390938b9f44327a
Bhushan <bhushangaykawad@gmail.com>
2022-07-23 19:07:45 +0530

    changed cmd-shift-o to OBS

--- hammerspoon/shortcuts/shortcuts.lua
+++ hammerspoon/shortcuts/shortcuts.lua
@@ -19,5 +19,5 @@ hs.hotkey.bind({ "cmd", "shift" }, "W", open_app({ "IntelliJ IDEA", "Webstorm" }
 hs.hotkey.bind({ "cmd", "shift" }, "P", open_app({ "Postman" }))
 
 hs.hotkey.bind({ "cmd", "shift" }, "M", open_app({ "Microsoft Teams", "Spotify" }))
-hs.hotkey.bind({ "cmd", "shift" }, "O", open_app({ "Microsoft Outlook", "Mail" }))
+hs.hotkey.bind({ "cmd", "shift" }, "O", open_app({ "Microsoft Outlook", "OBS" }))
 hs.hotkey.bind({ "cmd", "shift" }, "B", open_app({ "Google Chrome", "Brave Browser" }))
