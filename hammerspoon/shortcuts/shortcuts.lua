5b5627dbe88dc777512daf2dccb03466317ae7e3
Bhushan <bhushangaykawad@gmail.com>
2023-07-18 11:34:49 +0530

    removed PayPal related apps shortcuts

--- hammerspoon/shortcuts/shortcuts.lua
+++ hammerspoon/shortcuts/shortcuts.lua
@@ -10,14 +10,16 @@ local function open_app(names)
 end
 
 -- Hammerspoon will try to open applications with same order passed in as argument
+-- hs.hotkey.bind({ "cmd", "shift" }, "M", open_app({ "Microsoft Teams", "Spotify" }))
 -- If Microsoft Teams is opened then Spotify app will not open
 -- and if Microsoft Teams is not found then Spotify will be opened so on and so forth
+
 hs.hotkey.bind({ "cmd", "shift" }, "T", open_app({ "Kitty" }))
 hs.hotkey.bind({ "cmd", "shift" }, "S", open_app({ "Slack" }))
 hs.hotkey.bind({ "cmd", "shift" }, "D", open_app({ "Discord" }))
-hs.hotkey.bind({ "cmd", "shift" }, "W", open_app({ "Visual Studio Code", "IntelliJ IDEA", "Webstorm" }))
+hs.hotkey.bind({ "cmd", "shift" }, "W", open_app({ "Visual Studio Code", "PhpStorm", "Webstorm", "IntelliJ IDEA" }))
 hs.hotkey.bind({ "cmd", "shift" }, "P", open_app({ "Postman" }))
 
-hs.hotkey.bind({ "cmd", "shift" }, "M", open_app({ "Microsoft Teams", "Spotify" }))
-hs.hotkey.bind({ "cmd", "shift" }, "O", open_app({ "Microsoft Outlook", "OBS" }))
+hs.hotkey.bind({ "cmd", "shift" }, "M", open_app({ "Spotify" }))
+hs.hotkey.bind({ "cmd", "shift" }, "O", open_app({ "OBS" }))
 hs.hotkey.bind({ "cmd", "shift" }, "B", open_app({ "Google Chrome", "Brave Browser" }))
