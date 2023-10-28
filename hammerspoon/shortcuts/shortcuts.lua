7624f4d80c9a93fae8f1a63c6323339239222a58
Bhushan <bhushangaykawad@gmail.com>
2023-10-28 11:57:40 +0530

    spotify shortcut is back

--- hammerspoon/shortcuts/shortcuts.lua
+++ hammerspoon/shortcuts/shortcuts.lua
@@ -20,6 +20,6 @@ hs.hotkey.bind({ "cmd", "shift" }, "D", open_app({ "TablePlus" }))
 hs.hotkey.bind({ "cmd", "shift" }, "W", open_app({ "PhpStorm", "Webstorm", "Visual Studio Code", "IntelliJ IDEA" }))
 hs.hotkey.bind({ "cmd", "shift" }, "P", open_app({ "Postman" }))
 
--- hs.hotkey.bind({ "cmd", "shift" }, "M", open_app({ "Spotify" }))
+hs.hotkey.bind({ "cmd", "shift" }, "M", open_app({ "Spotify" }))
 -- hs.hotkey.bind({ "cmd", "shift" }, "O", open_app({ "OBS" }))
 hs.hotkey.bind({ "cmd", "shift" }, "B", open_app({ "Google Chrome", "Brave Browser" }))
