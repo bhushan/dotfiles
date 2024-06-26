48c3cfc11cb840f299dcff187afdad5e21ec9cfc
Bhushan <bhushangaykawad@gmail.com>
2024-06-26 06:50:28 +0530

    use arc for sometime

--- hammerspoon/shortcuts/shortcuts.lua
+++ hammerspoon/shortcuts/shortcuts.lua
@@ -22,4 +22,4 @@ hs.hotkey.bind({ "cmd", "shift" }, "P", open_app({ "Postman" }))
 
 hs.hotkey.bind({ "cmd", "shift" }, "M", open_app({ "Spotify" }))
 -- hs.hotkey.bind({ "cmd", "shift" }, "O", open_app({ "OBS" }))
-hs.hotkey.bind({ "cmd", "shift" }, "B", open_app({ "Google Chrome", "Brave Browser" }))
+hs.hotkey.bind({ "cmd", "shift" }, "B", open_app({ "Arc", "Google Chrome", "Brave Browser" }))
